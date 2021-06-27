ba5fa85b5bb872bc8108852d2c315b0779750571
Ramesh Meduri <ramesh.meduri@gmail.com>
2021-06-28 03:35:57 +0800

    init

--- C1/index.js
+++ C1/index.js
@@ -1,7 +1,11 @@
-import classes from './C1.module.css';
+import classNames from 'classnames/bind';
+import styles from './C1.module.css';
+const cx = classNames.bind(styles);
+
+console.log('cx --> ', cx('heading') );
 
 const C1 = () => {
-  return <h1 className={classes.heading}>Heading</h1>;
+  return <h1 className={cx('heading')}>Heading</h1>;
 };
 
 export default C1;
--- C2/index.js
+++ C2/index.js
@@ -1,7 +1,9 @@
-import classes from './C2.module.css';
+import classNames from 'classnames/bind';
+import styles from './C2.module.css';
+const cx = classNames.bind(styles);
 
 const C2 = () => {
-  return <h1 className={classes.heading}>Heading</h1>;
+  return <h1 className={cx('heading')}>Heading</h1>;
 };
 
 export default C2;
