792774a00de1d836d1a0d62b38cb22d114adc514
Ramesh Meduri <ramesh.meduri@gmail.com>
2021-06-28 02:24:06 +0800

    init

---
+++ C1/index.js
@@ -0,0 +1,7 @@
+import classes from './C1.module.css';
+
+const C1 = () => {
+  return <h1 className={classes.heading}>Heading</h1>;
+};
+
+export default C1;
---
+++ C2/index.js
@@ -0,0 +1,7 @@
+import classes from './C2.module.css';
+
+const C2 = () => {
+  return <h1 className={classes.heading}>Heading</h1>;
+};
+
+export default C2;
