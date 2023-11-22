a8de2af1b8cc28b1f690ab8a840d4577568d6e71
Ramesh Meduri <rameshmeduri@outlook.com>
2023-11-22 20:09:04 +0530

    init

--- README.md
+++
@@ -1,7 +0,0 @@
-# Tech Stack
-
-| package    | version |
-| ---------- | ------- |
-| react      | 15.6.2  |
-| bootstrap  | 4       |
-| reactstrap | 5       |
--- src/Components/C3/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './C3.module.css';
-
-const C3 = () => {
-  return <h1 className={styles.heading}>Heading</h1>;
-};
-
-export default C3;
